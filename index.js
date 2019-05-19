13dcfc8ef00176800959bebb09d0d4d781806149
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-05-19 19:58:05 +0200

    Cleanup and more serverless (patch)

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-const Router = require('router')
-const finalhandler = require('finalhandler')
-const cors = require('cors')
-const jwt = require('express-jwt')
-const secret = process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go'
+const url = require('url')
+const { json } = require('micro')
+const setStats = require('./lib/set-stats')
+const getStats = require('./lib/get-stats')
+const validateToken = require('./lib/validate-token')
+const logger = require('./lib/logger')
+let cache = {}
 
-// Utilities
-const handlers = require('./lib/handlers')
-const handleUnauthorized = require('./lib/handle-unauthorized')
+module.exports = async (request, response) => {
+  const pathname = url.parse(request.url).pathname
+  const id = pathname.split('/').pop().toLowerCase()
+  const method = request.method.toLowerCase()
+  let result = {}
 
-// Initialize a new router
-const router = Router()
-
-// CORS
-router.use(cors())
-
-// JWT
-router.use(jwt({ secret: secret }).unless({ path: [{ url: /\/stats/i, methods: ['GET'] }] }))
-router.use(handleUnauthorized)
-
-// ROUTES
-router.post('/stats/:id', handlers.setStats)
-router.get('/stats/:id', handlers.getStats)
-
-module.exports = (request, response) => {
-  router(request, response, finalhandler(request, response))
+  if (method === 'get') {
+    const cached = cache[id]
+    if (!cached) {
+      try {
+        result = await getStats(id)
+        cache[id] = result
+      } catch (error) {
+        logger('error', ['get', error])
+      }
+    } else {
+      result = cached
+    }
+  } else if (method === 'post') {
+    const bearerToken = request.headers.authorization
+    if (!bearerToken) {
+      const msg = 'missing Authorization header'
+      logger('warn', ['set', msg])
+      response.writeHead(401)
+      response.end('missing Authorization header')
+      return
+    }
+    try {
+      const token = bearerToken.replace('Bearer ', '')
+      await validateToken(token)
+    } catch (error) {
+      logger('error', ['token-auth', error])
+      response.writeHead(401)
+      response.end('invalid token in Authorization header')
+      return
+    }
+    try {
+      const data = await json(request)
+      const options = {
+        id: id,
+        data: data
+      }
+      result = await setStats(options)
+    } catch (error) {
+      logger('error', ['set', error])
+    }
+  }
+  response.writeHead(200)
+  response.end(JSON.stringify(result, null, 2))
 }
